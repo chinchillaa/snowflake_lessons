64d6430cc50a30aaae1015c4a4662f8119aef8e4
chinchillaa <2gawasa10ru@gmail.com>
2026-04-09 04:24:49 +0000

    add setup files

---
+++ util/setup_dora.sql
@@ -0,0 +1,41 @@
+-- RUN ALL --
+--  Create an API Integration
+
+use role accountadmin;
+
+create or replace api integration dora_api_integration
+api_provider = aws_api_gateway
+api_aws_role_arn = 'arn:aws:iam::321463406630:role/snowflakeLearnerAssumedRole'
+enabled = true
+api_allowed_prefixes = ('https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora');
+
+-- Create the GRADER Function
+
+use role accountadmin;  
+
+create or replace external function util_db.public.grader(
+      step varchar
+    , passed boolean
+    , actual integer
+    , expected integer
+    , description varchar)
+returns variant
+api_integration = dora_api_integration 
+context_headers = (current_timestamp, current_account, current_statement, current_account_name) 
+as 'https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora/grader'
+; 
+
+-- Is the GRADER Function working?
+
+use role accountadmin;
+use database util_db; 
+use schema public; 
+
+select grader(step, (actual = expected), actual, expected, description) as graded_results from
+(SELECT 
+ 'DORA_IS_WORKING' as step
+ ,(select 123) as actual
+ ,123 as expected
+ ,'Dora is working!' as description
+); 
+
